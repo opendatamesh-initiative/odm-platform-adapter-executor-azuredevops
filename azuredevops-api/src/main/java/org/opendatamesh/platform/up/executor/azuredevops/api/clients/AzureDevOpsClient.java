9a4bc97b9c60132129ea94ca33a300e5eb71d7b7
Mattia Pennati <mattia.pennati@quantyca.it>
2023-10-27 16:33:21 +0200

    Minor fixes

--- azuredevops-api/src/main/java/org/opendatamesh/platform/up/executor/azuredevops/api/clients/AzureDevOpsClient.java
+++ azuredevops-api/src/main/java/org/opendatamesh/platform/up/executor/azuredevops/api/clients/AzureDevOpsClient.java
@@ -1,14 +1,9 @@
 package org.opendatamesh.platform.up.executor.azuredevops.api.clients;
 
 import org.opendatamesh.platform.core.commons.clients.ODMClient;
-import org.opendatamesh.platform.core.commons.servers.exceptions.InternalServerException;
-import org.opendatamesh.platform.core.commons.servers.exceptions.UnprocessableEntityException;
 import org.opendatamesh.platform.core.dpds.ObjectMapperFactory;
-import org.opendatamesh.platform.up.executor.api.resources.ExecutorApiStandardErrors;
 import org.opendatamesh.platform.up.executor.azuredevops.api.components.OAuthTokenManager;
 import org.opendatamesh.platform.up.executor.azuredevops.api.resources.PipelineResource;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpEntity;
@@ -19,8 +14,6 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class AzureDevOpsClient extends ODMClient {
-
-    private static final Logger logger = LoggerFactory.getLogger(AzureDevOpsClient.class);
 
     public AzureDevOpsClient(@Value("${spring.security.oauth2.client.provider.azure-devops.basic-uri}") String serverAddress) {
         super(serverAddress, ObjectMapperFactory.JSON_MAPPER);
@@ -33,11 +26,8 @@ public class AzureDevOpsClient extends ODMClient {
         return String.format(pipelineUri, organization, project, pipelineId);
     }
 
-    public String runPipeline(PipelineResource pipelineResource, String organization, String project, String pipelineId){
-        if(organization == null || project == null || pipelineId == null)
-            throw new UnprocessableEntityException(
-                    ExecutorApiStandardErrors.SC422_05_TASK_IS_INVALID,
-                    "Impossible to send request to Azure DevOps: you must specify all parameters in template (organization, project and pipelineId)");
+    public ResponseEntity<String> runPipeline(PipelineResource pipelineResource, String organization, String project, String pipelineId){
+
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_JSON);
         headers.setBearerAuth(oAuthTokenManager.getToken());
@@ -47,13 +37,7 @@ public class AzureDevOpsClient extends ODMClient {
 
         ResponseEntity<String> response = rest.postForEntity(apiUrlFromString(pipelineUri), entity, String.class);
 
-        if(response.getStatusCode().is5xxServerError()){
-            throw new InternalServerException(
-                    ExecutorApiStandardErrors.SC502_50_REGISTRY_SERVICE_ERROR,
-                    "Azure DevOps responded with an internal server error: " + response.getBody());
-        }
+        return response;
 
-        logger.info("Pipeline run ended successfully");
-        return response.getBody();
     }
 }
--- azuredevops-server/src/main/java/org/opendatamesh/platform/up/executor/azuredevops/server/services/PipelineService.java
+++ azuredevops-server/src/main/java/org/opendatamesh/platform/up/executor/azuredevops/server/services/PipelineService.java
@@ -1,18 +1,18 @@
 package org.opendatamesh.platform.up.executor.azuredevops.server.services;
 
+import org.opendatamesh.platform.core.commons.servers.exceptions.InternalServerException;
+import org.opendatamesh.platform.core.commons.servers.exceptions.UnprocessableEntityException;
+import org.opendatamesh.platform.up.executor.api.resources.ExecutorApiStandardErrors;
 import org.opendatamesh.platform.up.executor.azuredevops.api.clients.AzureDevOpsClient;
-import org.opendatamesh.platform.up.executor.azuredevops.api.components.OAuthTokenManager;
-import org.opendatamesh.platform.up.executor.azuredevops.server.mappers.PipelineMapper;
 import org.opendatamesh.platform.up.executor.azuredevops.api.resources.PipelineResource;
+import org.opendatamesh.platform.up.executor.azuredevops.server.mappers.PipelineMapper;
 import org.opendatamesh.platform.up.executor.azuredevops.server.resources.odm.ConfigurationsResource;
 import org.opendatamesh.platform.up.executor.azuredevops.server.resources.odm.TemplateResource;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
 
 @Service
 public class PipelineService {
@@ -23,12 +23,35 @@ public class PipelineService {
     @Autowired
     protected AzureDevOpsClient azureDevOpsClient;
 
-
+    private static final Logger logger = LoggerFactory.getLogger(PipelineService.class);
 
     public String runPipeline(ConfigurationsResource configurationsResource, TemplateResource templateResource, String callbackRef) {
 
         PipelineResource pipelineResource = pipelineMapper.toAzurePipelineResource(configurationsResource,templateResource, callbackRef);
 
-        return azureDevOpsClient.runPipeline(pipelineResource, templateResource.getOrganization(), templateResource.getProject(), templateResource.getPipelineId());
+        if(
+                templateResource.getOrganization() == null ||
+                templateResource.getProject() == null ||
+                templateResource.getPipelineId() == null
+        )
+            throw new UnprocessableEntityException(
+                    ExecutorApiStandardErrors.SC422_05_TASK_IS_INVALID,
+                    "Impossible to send request to Azure DevOps: you must specify all parameters in template (organization, project and pipelineId)"
+            );
+
+        logger.info("Calling AzureDevOps to run the pipeline ...");
+
+        ResponseEntity<String> azureRespone = azureDevOpsClient.runPipeline(pipelineResource, templateResource.getOrganization(), templateResource.getProject(), templateResource.getPipelineId());
+        String azureResponseBody = azureRespone.getBody();
+
+        if(!azureRespone.getStatusCode().is2xxSuccessful()){
+            throw new InternalServerException(
+                    ExecutorApiStandardErrors.SC502_50_REGISTRY_SERVICE_ERROR,
+                    "Azure DevOps responded with an internal server error: " + azureResponseBody);
+        } else {
+            logger.info("Pipeline run ended successfully");
+            return azureResponseBody;
+        }
+
     }
 }
